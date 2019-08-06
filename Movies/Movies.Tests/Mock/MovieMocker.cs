9bcd8b1a3f3ded8c4c10ae5d22f5587995ed4250
Henrique <trianonhenrique@gmail.com>
2019-08-06 12:02:19 -0300

    Changed bogus Movie Dto Mocker
    Now it won't work in strict mode and it does not generate a random id

--- Movies/Movies.Tests/Mock/MovieMocker.cs
+++ Movies/Movies.Tests/Mock/MovieMocker.cs
@@ -6,20 +6,11 @@ namespace Movies.Tests.Mock
     public static class MovieMocker
     {
         private static readonly Faker<MovieDto> _faker = new Faker<MovieDto>()
-            //Ensure all properties have rules. By default, StrictMode is false
-            //Set a global policy by using Faker.DefaultStrictMode
-            .StrictMode(true)
-            //OrderId is deterministic
-            .RuleFor(o => o.Id, f => f.Random.Hash())
-            //Pick some fruit from a basket
+            .StrictMode(false)
             .RuleFor(o => o.Author, f => f.Name.FirstName())
-            //A random quantity from 1 to 10
             .RuleFor(o => o.Name, f => f.Commerce.ProductName())
-            //A nullable int? with 80% probability of being null.
-            //The .OrNull extension is in the Bogus.Extensions namespace.
             .RuleFor(o => o.Price, f => f.Random.Decimal(50, 100))
             .RuleFor(o => o.Category, f => f.Commerce.Categories(3));
-
         public static MovieDto Get() => _faker.Generate();
         
     }
