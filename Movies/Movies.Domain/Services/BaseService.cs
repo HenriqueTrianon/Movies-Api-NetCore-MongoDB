11aefe9e0295060edbf2cea4ecc938219b32cfbc
Henrique <trianonhenrique@gmail.com>
2019-08-07 17:45:36 -0300

    Removed some unusud namespace

--- Movies/Movies.Domain/Services/BaseService.cs
+++ Movies/Movies.Domain/Services/BaseService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using FluentValidation;
-using FluentValidation.Results;
 using Movies.Domain.Interfaces.Repository;
 using Movies.Domain.Interfaces.Services;
 using Movies.Infra.Core;
